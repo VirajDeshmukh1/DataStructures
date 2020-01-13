efacf5f13f5f1943c47a2dea1381e271c394503f
VirajDeshmukh1 <59378815+VirajDeshmukh1@users.noreply.github.com>
2020-01-12 18:47:50 -0800

    Add files via upload

---
+++ Arrays/Easy/ContainsDuplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
