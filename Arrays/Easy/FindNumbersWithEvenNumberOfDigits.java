efbb3e719fadb51488adeab4656366d8af870208
VirajDeshmukh1 <59378815+VirajDeshmukh1@users.noreply.github.com>
2020-01-17 19:33:27 -0800

    Add files via upload

---
+++ Arrays/Easy/FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>9 && nums[i]<100 || nums[i]>999 && nums[i]<10000){
+                count++;
+            }
+        }
+        return count;
+    }
+}
